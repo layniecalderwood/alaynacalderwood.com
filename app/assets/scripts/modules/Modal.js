e880b866a5eba169d82809818625227fe4026f2f
Laynie <alaynacalderwood1@gmail.com>
2020-09-17 10:25:01 -0600

    updates

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -33,7 +33,7 @@ class Modal {
         document.body.insertAdjacentHTML('beforeend', `
     <div class="modal">
     <div class="modal__inner">
-      <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon--modal-icon"> Get in <strong>Touch</strong></h2>
+      <h2 class="section-title section-title--blue section-title--less-margin">Get in <strong>Touch</strong></h2>
       <div class="wrapper wrapper--narrow">
         <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
       </div>
