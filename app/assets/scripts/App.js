a1528ba7ff7e49f77bf71f9ea3144912dd84e191
Laynie <alaynacalderwood1@gmail.com>
2020-11-11 16:53:15 -0700

    YAY

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,6 +8,7 @@ new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new MobileMenu();
+
 let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
